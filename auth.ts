c280cd397af2955e6f165ab781a0c5c954b4160d
Ricky Lee <rickyleedev@gmail.com>
2024-05-03 07:11:51 +0800

    .

--- auth.ts
+++ auth.ts
@@ -2,8 +2,20 @@ import NextAuth from 'next-auth'
 import authConfig from './auth.config'
 import { PrismaAdapter } from '@auth/prisma-adapter'
 import { db } from './lib/db'
+import { getUserByID } from './data/user'
 export const { handlers, signIn, signOut, auth } = NextAuth({
   ...authConfig,
-  adapter:PrismaAdapter(db),
-  session:{strategy:'jwt'}
+  adapter: PrismaAdapter(db),
+  session: { strategy: 'jwt' },
+  callbacks: {
+    async jwt({ token }) {
+      return token
+    },
+    async session({ token, session }) {
+      if(token.sub && session.user){
+        session.user.id=token.sub
+      }
+      return session
+    },
+  },
 })
